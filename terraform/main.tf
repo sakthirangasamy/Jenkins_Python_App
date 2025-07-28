7500a95c1c4383ec3c9536d69a787e5219d10217
sakthirangasamy <sakthirangasamy2003@gmail.com>
2025-07-28 11:13:18 +0530

    Commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,4 @@
-
-   provider "aws" {
+provider "aws" {
   region = "ap-southeast-2"
 }
 
@@ -35,7 +34,7 @@ resource "aws_security_group" "app_sg" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-010876b9ddd38475e" # Ubuntu 20.04 LTS
+  ami           = "ami-010876b9ddd38475e" # Ubuntu 20.04 LTS in ap-southeast-2
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
@@ -55,10 +54,16 @@ resource "aws_instance" "app_server" {
   }
 }
 
+# ✅ EXISTING OUTPUTS
 output "app_url" {
   value = "http://${aws_instance.app_server.public_ip}:8000"
 }
 
 output "ssh_command" {
   value = "ssh -i ~/.ssh/id_rsa ubuntu@${aws_instance.app_server.public_ip}"
+}
+
+# ✅ NEW OUTPUT FOR JENKINS PIPELINE
+output "instance_public_ip" {
+  value = aws_instance.app_server.public_ip
 }
